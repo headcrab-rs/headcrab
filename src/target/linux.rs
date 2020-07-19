d82034c67344c9fe35724afa72c6c3a31a9933ce
Nikita Baksalyar <nikita.baksalyar@gmail.com>
2020-07-19 03:12:29 +0100

    Implement a helper to read memory from multiple locations

--- src/target/linux.rs
+++ src/target/linux.rs
@@ -1,8 +1,7 @@
 use nix::{
     sys::ptrace,
-    sys::uio,
     sys::wait::waitpid,
-    unistd::{execv, fork, ForkResult, Pid},
+    unistd::{execv, fork, getpid, ForkResult, Pid},
 };
 use std::{
     ffi::CString,
@@ -11,11 +10,18 @@ use std::{
     mem,
 };
 
+/// This structure holds the state of a debuggee.
+/// You can use it to read & write debuggee's memory, pause it, set breakpoints, etc.
 pub struct Target {
     pid: Pid,
 }
 
 impl Target {
+    /// Uses this process as a debuggee.
+    pub fn me() -> Target {
+        Target { pid: getpid() }
+    }
+
     /// Launch a new debuggee process.
     /// Returns an opaque target handle which you can use to control the debuggee.
     pub fn launch(path: &str) -> Result<Target, Box<dyn std::error::Error>> {
@@ -49,35 +55,104 @@ impl Target {
         Ok(())
     }
 
-    /// Reads a string from the debuggee's memory.
-    // TODO: move this to a trait blanket impl
-    pub fn read_string(
-        &self,
-        base: usize,
-        len: usize,
-    ) -> Result<String, Box<dyn std::error::Error>> {
-        let mut read_buf = vec![0; len];
-        let buf_iov = uio::IoVec::from_mut_slice(&mut read_buf);
-        uio::process_vm_readv(self.pid, &[buf_iov], &[uio::RemoteIoVec { base, len }])?;
+    /// Reads memory from a debuggee process.
+    pub fn read(&self) -> ReadMemory {
+        ReadMemory::new(self.pid)
+    }
+}
 
-        Ok(String::from_utf8(read_buf)?)
+/// A single memory read operation.
+struct ReadOp {
+    // Remote memory location.
+    remote_base: usize,
+    // Size of the `local_ptr` buffer.
+    len: usize,
+    // Pointer to a local destination buffer.
+    local_ptr: *mut libc::c_void,
+}
+
+impl ReadOp {
+    /// Converts the memory read operation into a remote IoVec.
+    fn as_remote_iovec(&self) -> libc::iovec {
+        libc::iovec {
+            iov_base: self.remote_base as *const libc::c_void as *mut _,
+            iov_len: self.len,
+        }
     }
 
-    /// Reads pointer-sized data from the debuggee's memory.
-    /// The size of a result will be platform-dependent (32 or 64 bits).
-    // TODO: move this to a trait blanket impl
-    pub fn read_usize(&self, base: usize) -> Result<usize, Box<dyn std::error::Error>> {
-        let mut read_buf = [0; mem::size_of::<usize>()];
-        let buf_iov = uio::IoVec::from_mut_slice(&mut read_buf);
+    /// Converts the memory read operation into a local IoVec.
+    fn as_local_iovec(&self) -> libc::iovec {
+        libc::iovec {
+            iov_base: self.local_ptr,
+            iov_len: self.len,
+        }
+    }
+}
 
-        let remote_iov = uio::RemoteIoVec {
-            base,
-            len: mem::size_of::<usize>(),
+/// Allows to read memory from different locations in debuggee's memory as a single operation.
+/// On Linux, this will correspond to a single system call / context switch.
+pub struct ReadMemory {
+    pid: Pid,
+    read_ops: Vec<ReadOp>,
+}
+
+impl ReadMemory {
+    fn new(pid: Pid) -> ReadMemory {
+        ReadMemory {
+            pid,
+            read_ops: Vec::new(),
+        }
+    }
+
+    /// Reads a value of type `T` from debuggee's memory at location `remote_base`.
+    /// This value will be written to the provided variable `val`.
+    /// You should call `apply` in order to execute the memory read operation.
+    // todo: document mem safety - e.g., what happens in the case of partial read
+    pub fn read<T>(mut self, val: &mut T, remote_base: usize) -> Self {
+        self.read_ops.push(ReadOp {
+            remote_base,
+            len: mem::size_of::<T>(),
+            local_ptr: val as *mut T as *mut libc::c_void,
+        });
+
+        self
+    }
+
+    /// Executes the memory read operation.
+    pub fn apply(self) -> Result<(), Box<dyn std::error::Error>> {
+        // Create a list of `IoVec`s and remote `IoVec`s
+        let remote_iov = self
+            .read_ops
+            .iter()
+            .map(ReadOp::as_remote_iovec)
+            .collect::<Vec<_>>();
+
+        let local_iov = self
+            .read_ops
+            .iter()
+            .map(ReadOp::as_local_iovec)
+            .collect::<Vec<_>>();
+
+        let bytes_read = unsafe {
+            // todo: document unsafety
+            libc::process_vm_readv(
+                self.pid.into(),
+                local_iov.as_ptr(),
+                local_iov.len() as libc::c_ulong,
+                remote_iov.as_ptr(),
+                remote_iov.len() as libc::c_ulong,
+                0,
+            )
         };
 
-        uio::process_vm_readv(self.pid, &[buf_iov], &[remote_iov])?;
+        if bytes_read == -1 {
+            // fixme: return a proper error type
+            return Err(Box::new(nix::Error::last()));
+        }
 
-        Ok(usize::from_ne_bytes(read_buf))
+        // fixme: check that it's an expected number of read bytes and account for partial reads
+
+        Ok(())
     }
 }
 
@@ -94,4 +169,30 @@ pub fn get_addr_range(pid: Pid) -> Result<usize, Box<dyn std::error::Error>> {
     let addr_range: Vec<_> = proc_data[0].split('-').collect();
 
     Ok(usize::from_str_radix(addr_range[0], 16)?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ReadMemory;
+    use nix::unistd::getpid;
+
+    #[test]
+    fn read_memory() {
+        let var: usize = 52;
+        let var2: u8 = 128;
+
+        let mut read_var_op: usize = 0;
+        let mut read_var2_op: u8 = 0;
+
+        ReadMemory::new(getpid())
+            .read(&mut read_var_op, &var as *const _ as usize)
+            .read(&mut read_var2_op, &var2 as *const _ as usize)
+            .apply()
+            .expect("Failed to apply memop");
+
+        assert_eq!(read_var2_op, var2);
+        assert_eq!(read_var_op, var);
+
+        assert!(true);
+    }
 }
