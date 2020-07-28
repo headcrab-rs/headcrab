ab462fc35f5e89731b0cf0bf15c40707daf309f0
bjorn3 <bjorn3@users.noreply.github.com>
2020-07-28 22:53:17 +0200

    Add ADDR_NO_RANDOMIZE const
    Co-authored-by: Nikita Baksalyar <nikita.baksalyar@gmail.com>

--- src/target/unix.rs
+++ src/target/unix.rs
@@ -36,7 +36,8 @@ pub(crate) fn launch(path: &str) -> Result<Pid, Box<dyn std::error::Error>> {
             // Disable ASLR
             #[cfg(target_os = "linux")]
             unsafe {
-                libc::personality(0x0040000 /*ADDR_NO_RANDOMIZE*/);
+               const ADDR_NO_RANDOMIZE: usize = 0x0040000;
+               libc::personality(ADDR_NO_RANDOMIZE);
             }
 
             let path = CString::new(path)?;
