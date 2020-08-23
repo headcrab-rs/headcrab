30006ed22f569a6100b8ee70d5352a80d9fcdfe1
Jakub Jendryka <jendryka.j@gmail.com>
2020-08-24 00:56:31 +0200

    Added waiting for ack

--- src/target/gdb_remote.rs
+++ src/target/gdb_remote.rs
@@ -1,65 +1,162 @@
 use std::io::{Read, Write};
 
 pub struct GDBRemoteTarget<S: Read + Write> {
-    stream: S
+    stream: S,
+    timeout: Option<std::time::Duration>,
 }
 
 impl GDBRemoteTarget<std::net::TcpStream> {
-    pub fn connect_tcp<A: std::net::ToSocketAddrs>(addr: A) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn connect_tcp<A: std::net::ToSocketAddrs>(
+        addr: A,
+        timeout: std::time::Duration,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
         let stream = std::net::TcpStream::connect(addr)?;
-        Ok(Self { stream })
+        stream.set_read_timeout(Some(timeout))?;
+        stream.set_write_timeout(Some(timeout))?;
+        Ok(Self {
+            stream,
+            timeout: None,
+        })
     }
 }
+
+#[derive(Debug, Clone)]
+pub enum GDBRemoteTargetError {
+    InvalidCharacterReceived(char),
+    AckFailure(),
+    InvalidNumOfCharsRead(),
+}
+
+impl std::fmt::Display for GDBRemoteTargetError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let string = match self {
+            GDBRemoteTargetError::InvalidCharacterReceived(ch) => {
+                format!("Invalid character received: {}", ch)
+            }
+            GDBRemoteTargetError::AckFailure() => {
+                "Could not send data properly or ack was not received".to_string()
+            }
+            GDBRemoteTargetError::InvalidNumOfCharsRead() => {
+                "Read invalid number of chars".to_string()
+            }
+        };
+        write!(f, "{}", string)
+    }
+}
+
+impl std::error::Error for GDBRemoteTargetError {}
 
 impl<S: Read + Write> GDBRemoteTarget<S> {
+    pub fn send(&mut self, mut package: PacketWriter) -> Result<(), Box<dyn std::error::Error>> {
+        // FIXME: Move tries_left into struct
+        let mut tries_left = 5;
 
-    fn send_packet(&mut self, data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
-        let mut buffer = [0u8; 1024];
-        let len = encode_packet(data, &mut buffer);
+        while tries_left > 0 {
 
-        self.stream.write(&buffer[0..len])?;
-        self.stream.flush()?;
-        Ok(())
+            package.send(&mut self.stream)?;
+
+            let mut buf = [0u8; 1];
+            match self.stream.read(&mut buf) {
+                Ok(1) => match buf[0] as char {
+                    // Everything ok, received ack
+                    '+' => {
+                        return Ok(());
+                    }
+                    // Error occured during transmission, resending packet
+                    '-' => {
+                        package.send(&mut self.stream)?;
+                    }
+                    _ => {
+                        return Err(Box::new(GDBRemoteTargetError::InvalidCharacterReceived(
+                            buf[0] as char,
+                        )));
+                    }
+                },
+                // Received incorrect number of bytes
+                Ok(_) => {
+                    return Err(Box::new(GDBRemoteTargetError::InvalidNumOfCharsRead()));
+                }
+                Err(err) => {
+                    if let std::io::ErrorKind::TimedOut = err.kind() {
+                    } else {
+                        return Err(Box::new(err));
+                    }
+                }
+            }
+
+            // Waiting before trying to read again
+            if let Some(timeout) = self.timeout {
+                std::thread::sleep(timeout);
+            }
+            tries_left -= 1;
+        }
+
+        // Tries run out
+        Err(Box::new(GDBRemoteTargetError::AckFailure()))
     }
 }
 
-fn encode_packet(data: &[u8], buffer: &mut [u8]) -> usize {
+pub struct PacketWriter {
+    buffer: [u8; 1024],
+    pointer: usize,
+    checksum: u8,
+}
 
-    // Special character
-    buffer[0] = '$' as u8;
-    let mut buffer_index = 1;
-
-    // Escaping characters
-    for val in data {
-        if *val == '$' as u8 || *val == '#' as u8 || *val == '}' as u8 {
-            buffer[buffer_index] = '}' as u8;
-            buffer[buffer_index + 1] = val ^ 0x20;
-            buffer_index += 2;
-        }
-        else {
-            buffer[buffer_index] = *val;
-            buffer_index += 1;
+impl PacketWriter {
+    pub fn new() -> Self {
+        let mut buffer = [0u8; 1024];
+        buffer[0] = '$' as u8;
+        PacketWriter {
+            buffer,
+            pointer: 1,
+            checksum: 0,
         }
     }
 
-    // Special character
-    buffer[buffer_index] = '#' as u8;
-    buffer_index += 1;
-
-    // Calculating checksum
-    let remainder: u64 = data.iter().fold(0, |acc, x| {
-        let res = acc + (*x as u64);
-        res % 256
-    });
-
-    // Inserting checksum
-    let checksum = format!("{:x}", remainder);
-    for ch in checksum.chars(){
-        buffer[buffer_index] = ch as u8;
-        buffer_index += 1;
+    fn write_raw(&mut self, byte: u8) {
+        self.buffer[self.pointer] = byte;
+        self.pointer += 1;
     }
 
-    buffer_index
+    pub fn write_byte(mut self, byte: u8) -> Self {
+        if byte == '$' as u8 || byte == '#' as u8 || byte == '}' as u8 {
+            self.write_raw('}' as u8);
+            self.write_raw(byte ^ 0x20);
+            self.checksum = self.checksum.wrapping_add('}' as u8);
+            self.checksum = self.checksum.wrapping_add(byte ^ 0x20);
+        } else {
+            self.write_raw(byte);
+            self.checksum = self.checksum.wrapping_add(byte);
+        }
+        self
+    }
+
+    pub fn write_slice(self, data: &[u8]) -> Self {
+        data.iter()
+            .fold(self, |packet_writer, byte| packet_writer.write_byte(*byte))
+    }
+
+    pub fn write_str(self, s: &str) -> Self {
+        self.write_slice(s.as_bytes())
+    }
+
+    fn add_checksum(&mut self) {
+        self.write_raw('#' as u8);
+        let checksum_str = format!("{:02x}", self.checksum);
+        for ch in checksum_str.chars() {
+            self.write_raw(ch as u8);
+        };
+    }
+
+    pub fn send<W: std::io::Write>(
+        &mut self,
+        writer: &mut W,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.add_checksum();
+        writer.write(&self.buffer[0..self.pointer])?;
+        writer.flush()?;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -68,21 +165,18 @@ mod tests {
 
     #[test]
     fn encode_packet_test() {
-        let expected = "$OK#9a";
-        let mut buffer = [0u8; 6];
-        let data = ['O' as u8, 'K' as u8];
-        let len = encode_packet(&data, &mut buffer);
-        assert_eq!(6, len);
-        assert_eq!(expected, std::str::from_utf8(&buffer).unwrap());
-    }
+        let mut packet_writer = PacketWriter::new().write_str("OK");
+        packet_writer.add_checksum();
+        assert_eq!(
+            "$OK#9a",
+            std::str::from_utf8(&packet_writer.buffer[0..packet_writer.pointer]).unwrap()
+        );
 
-    #[test]
-    fn encode_packet_escape_test() {
-        let expected = "$}]#7d";
-        let mut buffer = [0u8; 6];
-        let data = ['}' as u8];
-        let len = encode_packet(&data, &mut buffer);
-        assert_eq!(6, len);
-        assert_eq!(expected, std::str::from_utf8(&buffer).unwrap());
+        let mut packet_writer = PacketWriter::new().write_str("}");
+        packet_writer.add_checksum();
+        assert_eq!(
+            "$}]#da",
+            std::str::from_utf8(&packet_writer.buffer[0..packet_writer.pointer]).unwrap()
+        );
     }
 }
