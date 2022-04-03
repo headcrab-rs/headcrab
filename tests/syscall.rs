1e9e136fcd9e84dc108f230e13c73b576a7584fe
Proloy Mishra <67726964+pro465@users.noreply.github.com>
2022-04-03 21:53:55 +0530

    Update syscall.rs

--- tests/syscall.rs
+++ tests/syscall.rs
@@ -41,7 +41,7 @@ fn syscall() -> CrabResult<()> {
             // unmap the previously mapped memory
             // and check that it is no longer in the mapped memory list.
             target.munmap(addr as *mut _, len)?;
-            assert!(!target.memory_maps()?.contains(&addr));
+            assert!(target.memory_maps()?.iter().all(|map| map.address.0 != addr);
 
             return Ok(());
         }
