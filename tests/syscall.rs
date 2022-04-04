ae5e5260f885bf99eba0f035df97d5925f0ff6bb
Proloy Mishra <67726964+pro465@users.noreply.github.com>
2022-04-04 20:11:51 +0530

    add m[un]map tests (#136)
    * add m[un]map tests
    * simplify syscall.rs tests
    
    Co-authored-by: bjorn3 <bjorn3@users.noreply.github.com>

--- tests/syscall.rs
+++ tests/syscall.rs
@@ -20,27 +20,34 @@ fn syscall() -> CrabResult<()> {
         std::fs::read_to_string(format!("/proc/{}/maps", target.pid()))?
     );
 
+    let len = 1 << 20;
     let addr = target
         .mmap(
             0 as *mut _,
-            1 << 20,
+            len,
             libc::PROT_READ | libc::PROT_WRITE,
             libc::MAP_ANONYMOUS | libc::MAP_PRIVATE,
             0,
             0,
         )
         .unwrap();
+    assert!(target
+        .memory_maps()?
+        .iter()
+        .any(|map| map.address.0 == addr));
 
-    for line in std::fs::read_to_string(format!("/proc/{}/maps", target.pid()))?.lines() {
-        if line.starts_with(&format!("{:08x}-", addr)) {
-            // Found mapped addr
-            test_utils::continue_to_end(&target);
-            return Ok(());
-        }
-    }
+    // unmap the previously mapped memory
+    // and check that it is no longer in the mapped memory list.
+    dbg!("munmapping...");
+    target.munmap(addr as *mut _, len)?;
+    dbg!("munmapped!");
 
-    panic!(
-        "\n{}\n",
-        std::fs::read_to_string(format!("/proc/{}/maps", target.pid()))?
-    );
+    assert!(target
+        .memory_maps()?
+        .iter()
+        .all(|map| map.address.0 != addr));
+
+    test_utils::continue_to_end(&target);
+
+    Ok(())
 }
